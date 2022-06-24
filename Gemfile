fb75060acabaae03e9e3f7882339e96e1038eb3b
vip <vip1313@gmail.com>
2022-06-24 15:25:27 +0300

    create project

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+ruby '3.0.3'
+gem 'rails', '~> 6.1.6'
+gem 'pg', '~> 1.1'
+gem 'puma', '~> 5.0'
+gem 'sass-rails', '>= 6'
+gem 'webpacker', '~> 5.0'#
+gem 'bootsnap', '>= 1.4.4', require: false
+
+group :development, :test do
+  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+end
+
+group :development do
+  gem 'web-console', '>= 4.1.0'
+  gem 'listen', '~> 3.3'
+end
+
+
---
+++ config/application.rb
@@ -0,0 +1,39 @@
+require_relative "boot"
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+# require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require "action_view/railtie"
+# require "action_cable/engine"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module News
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.1
+    config.i18n.available_locales = %i[en ru]
+    config.i18n.default_locale = :ru
+    config.time_zone = 'Kyiv'
+    config.generators.system_tests = nil
+    config.generators do |g|
+      g.orm              :active_record
+      g.template_engine  :slim
+      g.test_framework   nil
+      g.helper           false
+      g.stylesheets      false
+      g.javascripts      false
+    end
+  end
+end
