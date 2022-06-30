ef3a67b9607dc55f4edd2afdaecef7ae028e8bb2
vip <vip1313@gmail.com>
2022-06-30 23:18:28 +0300

    fix delete post

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,20 +6,30 @@ import "trix"
 import "@rails/actiontext"
 import Rails from "@rails/ujs"
 import  'bootstrap' 
-import  './stylesheets/application.scss'
+import { Tooltip, Popover } from "bootstrap"
 
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("bootstrap");
-
-var jQuery = require("jquery");
-// import jQuery from "jquery";
-global.$ = global.jQuery = jQuery;
-window.$ = window.jQuery = jQuery;
+require("bootstrap")
+require("trix")
+require("@rails/actiontext")
+require("@popperjs/core")
+require("../stylesheets/application.scss")
 
 Rails.start()
 
-require("trix")
-require("@rails/actiontext")
+
+document.addEventListener("turbolinks:load", () => {
+
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+        return new Tooltip(tooltipTriggerEl)
+    })
+
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+        return new Popover(popoverTriggerEl)
+    })
+})
