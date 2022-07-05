bbed9971d8737fb929ed168263e1096faa8a3921
vip <vip1313@gmail.com>
2022-07-05 18:32:55 +0300

    add font_awesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+
 import "trix"
 import "@rails/actiontext"
 import Rails from "@rails/ujs"
@@ -16,6 +17,7 @@ require("trix")
 require("@rails/actiontext")
 require("@popperjs/core")
 require("../stylesheets/application.scss")
+
 
 Rails.start()
 
